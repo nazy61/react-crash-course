6d2ae2ddc9b7943d96e95b77a8bed7d639c0d117
Ogwo Chinaza <meabout9@gmail.com>
2021-03-10 13:13:41 +0100

    react crash course almost done

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import PropTypes from "prop-types";
+import Button from "./Button";
+
+const Header = ({ title, toggleAdd, showAdd }) => {
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <Button
+        onClick={toggleAdd}
+        color={showAdd ? "red" : "green"}
+        text={showAdd ? "Close" : "Add"}
+      />
+    </header>
+  );
+};
+
+Header.defaultProps = {
+  title: "Task Tracker",
+};
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+// CSS in JS
+// const headingStyle = {
+//     color: 'red',
+//     backgroundColor: 'black'
+// }
+
+export default Header;
